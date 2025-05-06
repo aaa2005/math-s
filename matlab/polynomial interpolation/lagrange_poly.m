32300beacb389affb7de7d21b35dd65b7459dccb
aaa2005 <aaa2005games@gmail.com>
2025-05-06 01:21:20 +0100

    Create lagrange_poly.m

---
+++ matlab/polynomial interpolation/lagrange_poly.m
@@ -0,0 +1,17 @@
+function Pn = lagrange_poly(A, x)
+    syms x # add this to see the polynomial
+    n = length(A);
+    f = @(t) exp(t);
+    S = 0;
+    for i = 1:n
+      L = 1;
+      for j = 1:n
+        if j ~= i
+          L = L * (x - A(j)) / (A(i) - A(j));
+        end
+      end
+      Ln = L * f(A(i));
+      S = S + Ln;
+    end
+    Pn = S;
+end
